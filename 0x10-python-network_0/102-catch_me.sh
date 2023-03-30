6f56c63ab4b0a8ab749fbfbcb1db97eff3c90bf9
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2023-03-30 20:14:05 +0100

    python network 0 -2

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me -X PUT -d "user_id=98"  -H "Origin: You got me!"
+curl -s 0.0.0.0:5000/catch_me -X PUT -L -d "user_id=98" -H "Origin: School"
