32f81e33a8171f59eda929561626a953618e346a
Ugo Uvietesivwi <uvie.ugo@gmail.com>
2023-03-31 16:33:21 +0100

    python-network_1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,8 +8,5 @@ import sys
 
 
 if __name__ == "__main__":
-    try:
-        r = requests.get(sys.argv[1])
-        print(r.headers['X-Request-Id'])
-    except ValueError:
-        pass
+    html = get(argv[1])
+    print(html.headers.get('X-Request-Id'))
